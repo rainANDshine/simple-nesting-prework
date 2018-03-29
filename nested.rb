35ecc003a1247ad1065ed4e93f728f1295399fa9
Shun Yao <ysmiracle@hotmail.com>
2018-03-29 03:38:22 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -88,6 +88,8 @@ def adding_matz
     :known_for => "Ruby",
     :languages => ["LISP, C"]
   }
+  
+  programmer_hash
 end
 
 def changing_alan
@@ -106,10 +108,11 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     programmer_hash[:alan_kay][:known_for] = alans_new_info
-     
+  #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
+  alans_new_info = "GUI"
+  programmer_hash[:alan_kay][:known_for] = alans_new_info
+  
+  programmer_hash   
 end
 
 def adding_to_dennis
@@ -130,4 +133,6 @@ def adding_to_dennis
      }
   
   programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  
+  programmer_hash
 end
